dcf5bfff5dd9ebd050ba7baa22ee6e3558d578a1
Luiz Hatem <aminhatem80@gmail.com>
2024-01-13 19:40:46 -0300

    Envio do projeto.

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Existem vários tipos de variáveis e a linguagem Java é altamente tipada, o que ajuda na organização
+        // temos os tipos de variáveis short, char, long, int, double, float, byte e boolean
+        //Exemplos:
+        int numInt = 1; //um número inteiro.
+        double numReal = 2.4; //um número real
+        boolean bool = true;// Um valor lógico (true ou false).
+        char caractere = 'A'; //Apenas um caracter é aceito.
+        String texto = "Luiz Hatem";
+        // e etc. Por fim podemos ver acima alguns exemplos dos tipos de variáveis mais usados na linguagem Java.
+        
     }
 }
